fcef396a312cdc731711918c22fd7dba2601542a
joselitodasilva <joselito.dasilva32@gmail.com>
2017-07-15 03:22:50 +0100

    converting junkberry from class to module

--- junkberry.py
+++ junkberry.py
@@ -1,14 +1,24 @@
 import sys
 import oscserver
 
-class junkberry():
-	def defineType(self):
-		self.type = sys.platform
+device = sys.platform
 
-	def osc(self, command):
-		if command == "run":
-			oscserver.start_osc()
-		elif command == "stop":
-			oscserver.stop_osc()
-		else:
-			print "Command not recognize. Running: {}".format(oscserver.running)
+def osc(command):
+	if command == "run":
+		oscserver.start_osc()
+	elif command == "stop":
+		oscserver.stop_osc()
+	else:
+		print "Command not recognize. Running: {}".format(oscserver.running)
+
+#class junkberry:
+#	def defineType(self):
+#		self.type = sys.platform
+#
+#	def osc(self, command):
+#		if command == "run":
+#			oscserver.start_osc()
+#		elif command == "stop":
+#			oscserver.stop_osc()
+#		else:
+#			print "Command not recognize. Running: {}".format(oscserver.running)
